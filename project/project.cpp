16c4b8eda1ad309dab727199b5aa8ccab6d6b461
Vahe Danielyan <danielyan.vahe@gmail.com>
2023-03-19 23:29:59 +0400

    Rearrange headers, add version reporting over swo

--- project/project.cpp
+++ project/project.cpp
@@ -1,22 +1,19 @@
+#include <stdio.h>
+
 #include <bsp.h>
 #include <params.h>
+#include <version.h>
 
-/* HAL */
 #include <gpio.h>
-
-/* Utils */
 #include <crc.h>
 #include <swo.h>
-
-/* Drivers */
 #include <dummy.h>
-
-#include <stdio.h>
 
 void bsp_main()
 {
     swo_init();
     hal::GPIO::configure(LED_BUILTIN_GPIO_Port, LED_BUILTIN_Pin, hal::GPIOInit());
+    printf(VERSION);
     while (1) {
         static size_t counter = 0;
         printf("Counter: %d\n", ++counter);
---
+++ project/version.h
@@ -0,0 +1,6 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION "STM32 Template Project, built on STM32F407VET6 v1.0.0_beta1\n"
+
+#endif
